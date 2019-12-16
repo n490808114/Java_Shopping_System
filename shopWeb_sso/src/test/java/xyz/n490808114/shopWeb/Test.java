f829aedf0b764b5f05ad07730edee12948eaaa8e
zjh <eamil@qq.com>
2019-12-16 18:04:18 +0800

    add

--- shopWeb_sso/src/test/java/xyz/n490808114/shopWeb/Test.java
+++ shopWeb_sso/src/test/java/xyz/n490808114/shopWeb/Test.java
@@ -1,14 +1,25 @@
 package xyz.n490808114.shopWeb;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 public class Test{
     public static void main(String[] args) {
         Long count = 6L;
         System.out.println(count == null || count <= 5);
     }
 
+}
+
+interface A{
+    default void a(){
+        System.out.println("1");
+    };
+}
+interface B{
+    default void a(){
+        System.out.println("2");
+    };
+}
+class C implements A,B{
+    public void a() {
+        A.super.a();
+    }
 }
