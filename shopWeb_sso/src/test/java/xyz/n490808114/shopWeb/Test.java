0b15b45a832845ba9b46917a2d377fb988042e2e
zjh <eamil@qq.com>
2019-12-16 17:01:24 +0800

    add

--- shopWeb_sso/src/test/java/xyz/n490808114/shopWeb/Test.java
+++ shopWeb_sso/src/test/java/xyz/n490808114/shopWeb/Test.java
@@ -1,12 +1,14 @@
 package xyz.n490808114.shopWeb;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Test{
     public static void main(String[] args) {
-        Integer i = 1;
-        Map<String,String> a = new HashMap<>();
+        Long count = 6L;
+        System.out.println(count == null || count <= 5);
     }
 
 }
